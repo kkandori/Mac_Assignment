bb3e3263ac02b52fd670c5ce1185d9b7c087d252
kkandori <kkandori@gmail.com>
2013-12-03 16:02:46 +0900

    !A initial commit

---
+++ Assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment
+//
+//  Created by DK on 12/2/13.
+//  Copyright (c) 2013 DK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AssignmentTests/AssignmentTests.m
@@ -0,0 +1,34 @@
+//
+//  AssignmentTests.m
+//  AssignmentTests
+//
+//  Created by DK on 12/2/13.
+//  Copyright (c) 2013 DK. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AssignmentTests : XCTestCase
+
+@end
+
+@implementation AssignmentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
